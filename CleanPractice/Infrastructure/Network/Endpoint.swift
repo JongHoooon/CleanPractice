85811917f7c85905573c0c5b88930950f32907f5
JongHoon <qnm541@gmail.com>
2023-08-21 20:54:18 +0900

    network infra 파일들 Infrastructure 폴더로 이동

---
+++ CleanPractice/Infrastructure/Network/Endpoint.swift
@@ -0,0 +1,11 @@
+//
+//  Endpoint.swift
+//  CleanPractice
+//
+//  Created by JongHoon on 2023/08/21.
+//
+
+enum Endpoint {
+    static var baseURL = "https://api.themoviedb.org/3"
+    static var imageBaseURL = "https://image.tmdb.org/t/p/w500"
+}
