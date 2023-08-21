d6a80a81d0a6cd18ae6e97713e1997f3a91d72ac
JongHoon <qnm541@gmail.com>
2023-08-21 17:32:35 +0900

    add Entity

---
+++ CleanPractice/Domain/Entities/Video.swift
@@ -0,0 +1,14 @@
+//
+//  Video.swift
+//  CleanPractice
+//
+//  Created by JongHoon on 2023/08/21.
+//
+
+struct Video {
+    let id: Int
+    let backdropPath: String
+    let title: String
+    let voteAverage: Double
+    let releaseDate: String
+}
---
+++ CleanPractice/Domain/Entities/VideoDetail.swift
@@ -0,0 +1,17 @@
+//
+//  VideoDetail.swift
+//  CleanPractice
+//
+//  Created by JongHoon on 2023/08/21.
+//
+
+struct VideoDetail {
+    let id: Int
+    let backdropPath: String
+    let genres: [String]
+    let title: String
+    let overView: String
+    let runtime: Int
+    let voteAverage: Float
+    let releaseDate: String
+}
