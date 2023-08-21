f39bd6d37bf8ae0b8ebcfbbd02752b2d35a068f4
JongHoon <qnm541@gmail.com>
2023-08-21 21:53:35 +0900

    [feature] DefaultMovieRepository 추가

---
+++ CleanPractice/Data/Repositories/DefaultMovieRepository.swift
@@ -0,0 +1,38 @@
+//
+//  DefaultMovieRepository.swift
+//  CleanPractice
+//
+//  Created by JongHoon on 2023/08/21.
+//
+
+import Foundation
+
+final class DefaultMovieRepository: MovieRepository, CallRequestable {
+    typealias APIType = VideoAPI
+    
+    func fetchPopular(page: Int) async throws -> VideosPage {
+        do {
+            let popularMoviesDTO = try await callRequest(
+                of: PopularMoviesDTO.self,
+                movieAPI: .fetchPopularMovies(page: page)
+            )
+            
+            return popularMoviesDTO.toDomain()
+        } catch {
+            throw error
+        }
+    }
+
+    func fetchDetail(id: Int) async throws -> VideoDetail {
+        do {
+            let movieDetailDTO = try await callRequest(
+                of: VideoDetailDTO.self,
+                movieAPI: .fetchMovieDetail(id: id)
+            )
+            
+            return movieDetailDTO.toDomain()
+        } catch {
+            throw error
+        }
+    }
+}
