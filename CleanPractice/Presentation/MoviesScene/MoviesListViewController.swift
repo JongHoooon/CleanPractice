d3aa48c156a1b43abed539f4f2177b80f19dd15b
JongHoon <qnm541@gmail.com>
2023-08-21 17:06:27 +0900

    MoviesListViewController 생성

---
+++ CleanPractice/Presentation/MoviesScene/MoviesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesListViewController.swift
+//  CleanPractice
+//
+//  Created by JongHoon on 2023/08/21.
+//
+
+import UIKit
+
+final class MoviesListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
